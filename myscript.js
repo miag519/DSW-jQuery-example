e8a510ab5f562680f51dbb09cefa47a2439381f6
miag519 <miag1984.5@gmail.com>
2024-02-08 14:21:13 -0800

    added animation & hover color

--- myscript.js
+++ myscript.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
 	});
+	$("button").click(function(){
+    $("div").animate({
+      height: 'toggle'
+    });
+	});
+	$("p").hover(function(){
+      $(this).css("background-color", "green");
+    });
+	
+	
 });
